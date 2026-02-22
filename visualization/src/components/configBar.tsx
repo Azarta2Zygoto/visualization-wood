c14dd3a26ce56653f15f03a9011ecc9ad668a366
Azarta2Zygoto <quentin.potiron@gmail.com>
2026-02-22 17:12:16 +0100

    Correction CSS

--- visualization/src/components/configBar.tsx
+++ visualization/src/components/configBar.tsx
@@ -152,214 +152,220 @@ export default function ConfigBar({
                 </button>
             </div>
             <p>{t("data-type")}</p>
-            <div
-                className="rows"
-                style={{ gap: 0 }}
-            >
-                <button
-                    className={`btn ${typeData === 0 || typeData === 2 ? "active" : ""}`}
-                    type="button"
-                    onClick={() => handleTypeDataChange(0, isVolume)}
-                    aria-label={t("export-desc")}
-                    title={t("export-desc")}
-                    style={{
-                        width: "clamp(60px, 10vw, 120px)",
-                        borderTopRightRadius: 0,
-                        borderBottomLeftRadius: 0,
-                        borderBottomRightRadius: 0,
-                    }}
+            <div>
+                <div
+                    className="rows"
+                    style={{ gap: 0 }}
                 >
-                    {t("export")}
-                </button>
-                <button
-                    className={`btn ${typeData === 1 || typeData === 3 ? "active" : ""}`}
-                    type="button"
-                    onClick={() => handleTypeDataChange(1, isVolume)}
-                    aria-label={t("import-desc")}
-                    title={t("import-desc")}
-                    style={{
-                        borderRadius: 0,
-                        width: "clamp(60px, 10vw, 120px)",
-                        borderLeft: "transparent",
-                    }}
-                >
-                    {t("import")}
-                </button>
-                <button
-                    className={`btn ${typeData === 4 ? "active" : ""}`}
-                    type="button"
-                    onClick={() => handleTypeDataChange(4, isVolume)}
-                    aria-label={t("balance-desc")}
-                    title={t("balance-desc")}
-                    style={{
-                        width: "clamp(60px, 10vw, 120px)",
-                        borderLeftColor: "transparent",
-                        borderTopLeftRadius: 0,
-                        borderBottomLeftRadius: 0,
-                        borderBottomRightRadius: 0,
-                    }}
-                >
-                    {t("balance")}
-                </button>
+                    <button
+                        className={`btn ${typeData === 0 || typeData === 2 ? "active" : ""}`}
+                        type="button"
+                        onClick={() => handleTypeDataChange(0, isVolume)}
+                        aria-label={t("export-desc")}
+                        title={t("export-desc")}
+                        style={{
+                            width: "clamp(60px, 10vw, 120px)",
+                            borderTopRightRadius: 0,
+                            borderBottomLeftRadius: 0,
+                            borderBottomRightRadius: 0,
+                        }}
+                    >
+                        {t("export")}
+                    </button>
+                    <button
+                        className={`btn ${typeData === 1 || typeData === 3 ? "active" : ""}`}
+                        type="button"
+                        onClick={() => handleTypeDataChange(1, isVolume)}
+                        aria-label={t("import-desc")}
+                        title={t("import-desc")}
+                        style={{
+                            borderRadius: 0,
+                            width: "clamp(60px, 10vw, 120px)",
+                            borderLeft: "transparent",
+                        }}
+                    >
+                        {t("import")}
+                    </button>
+                    <button
+                        className={`btn ${typeData === 4 ? "active" : ""}`}
+                        type="button"
+                        onClick={() => handleTypeDataChange(4, isVolume)}
+                        aria-label={t("balance-desc")}
+                        title={t("balance-desc")}
+                        style={{
+                            width: "clamp(60px, 10vw, 120px)",
+                            borderLeftColor: "transparent",
+                            borderTopLeftRadius: 0,
+                            borderBottomLeftRadius: 0,
+                            borderBottomRightRadius: 0,
+                        }}
+                    >
+                        {t("balance")}
+                    </button>
+                </div>
+                {typeData !== 4 ? (
+                    <div
+                        className="rows"
+                        style={{ gap: 0 }}
+                    >
+                        <button
+                            className={`btn ${typeData === 0 || typeData === 1 ? "active" : ""}`}
+                            type="button"
+                            disabled={typeData === 4}
+                            onClick={() => handleTypeDataChange(typeData, true)}
+                            aria-label={t("volume-desc")}
+                            title={t("volume-desc")}
+                            style={{
+                                width: "clamp(90px, 15vw, 180px)",
+                                borderTopColor: "transparent",
+                                borderTopRightRadius: 0,
+                                borderTopLeftRadius: 0,
+                                borderBottomRightRadius: 0,
+                            }}
+                        >
+                            {t("volume")}
+                        </button>
+                        <button
+                            className={`btn ${typeData === 2 || typeData === 3 ? "active" : ""}`}
+                            type="button"
+                            onClick={() =>
+                                handleTypeDataChange(typeData, false)
+                            }
+                            aria-label={t("value-desc")}
+                            title={t("value-desc")}
+                            style={{
+                                width: "clamp(90px, 15vw, 180px)",
+                                borderLeftColor: "transparent",
+                                borderTopColor: "transparent",
+                                borderTopLeftRadius: 0,
+                                borderTopRightRadius: 0,
+                                borderBottomLeftRadius: 0,
+                            }}
+                        >
+                            {t("value")}
+                        </button>
+                    </div>
+                ) : (
+                    <div
+                        className="rows"
+                        style={{ gap: 0 }}
+                    >
+                        <button
+                            className={`btn ${isAbsolute ? "active" : ""}`}
+                            type="button"
+                            onClick={() => setIsAbsolute(true)}
+                            aria-label={t("value-absolute-desc")}
+                            title={t("value-absolute-desc")}
+                            style={{
+                                width: "clamp(90px, 15vw, 180px)",
+                                borderTopColor: "transparent",
+                                borderTopRightRadius: 0,
+                                borderTopLeftRadius: 0,
+                                borderBottomRightRadius: 0,
+                            }}
+                        >
+                            {t("value-absolute")}
+                        </button>
+                        <button
+                            className={`btn ${!isAbsolute ? "active" : ""}`}
+                            type="button"
+                            onClick={() => setIsAbsolute(false)}
+                            aria-label={t("value-relative-desc")}
+                            title={t("value-relative-desc")}
+                            style={{
+                                width: "clamp(90px, 15vw, 180px)",
+                                borderLeftColor: "transparent",
+                                borderTopColor: "transparent",
+                                borderTopLeftRadius: 0,
+                                borderTopRightRadius: 0,
+                                borderBottomLeftRadius: 0,
+                            }}
+                        >
+                            {t("value-relative")}
+                        </button>
+                    </div>
+                )}
             </div>
-            {typeData !== 4 ? (
-                <div
-                    className="rows"
-                    style={{ gap: 0 }}
-                >
-                    <button
-                        className={`btn ${typeData === 0 || typeData === 1 ? "active" : ""}`}
-                        type="button"
-                        disabled={typeData === 4}
-                        onClick={() => handleTypeDataChange(typeData, true)}
-                        aria-label={t("volume-desc")}
-                        title={t("volume-desc")}
-                        style={{
-                            width: "clamp(90px, 15vw, 180px)",
-                            borderTopColor: "transparent",
-                            borderTopRightRadius: 0,
-                            borderTopLeftRadius: 0,
-                            borderBottomRightRadius: 0,
-                        }}
-                    >
-                        {t("volume")}
-                    </button>
-                    <button
-                        className={`btn ${typeData === 2 || typeData === 3 ? "active" : ""}`}
-                        type="button"
-                        onClick={() => handleTypeDataChange(typeData, false)}
-                        aria-label={t("value-desc")}
-                        title={t("value-desc")}
-                        style={{
-                            width: "clamp(90px, 15vw, 180px)",
-                            borderLeftColor: "transparent",
-                            borderTopColor: "transparent",
-                            borderTopLeftRadius: 0,
-                            borderTopRightRadius: 0,
-                            borderBottomLeftRadius: 0,
-                        }}
-                    >
-                        {t("value")}
-                    </button>
-                </div>
-            ) : (
-                <div
-                    className="rows"
-                    style={{ gap: 0 }}
-                >
-                    <button
-                        className={`btn ${isAbsolute ? "active" : ""}`}
-                        type="button"
-                        onClick={() => setIsAbsolute(true)}
-                        aria-label={t("value-absolute-desc")}
-                        title={t("value-absolute-desc")}
-                        style={{
-                            width: "clamp(90px, 15vw, 180px)",
-                            borderTopColor: "transparent",
-                            borderTopRightRadius: 0,
-                            borderTopLeftRadius: 0,
-                            borderBottomRightRadius: 0,
-                        }}
-                    >
-                        {t("value-absolute")}
-                    </button>
-                    <button
-                        className={`btn ${!isAbsolute ? "active" : ""}`}
-                        type="button"
-                        onClick={() => setIsAbsolute(false)}
-                        aria-label={t("value-relative-desc")}
-                        title={t("value-relative-desc")}
-                        style={{
-                            width: "clamp(90px, 15vw, 180px)",
-                            borderLeftColor: "transparent",
-                            borderTopColor: "transparent",
-                            borderTopLeftRadius: 0,
-                            borderTopRightRadius: 0,
-                            borderBottomLeftRadius: 0,
-                        }}
-                    >
-                        {t("value-relative")}
-                    </button>
-                </div>
-            )}
 
             <p>{t("date-choose")}</p>
-            <div
-                className="rows"
-                style={{ gap: 0 }}
-            >
-                <button
-                    className="btn"
-                    type="button"
-                    aria-label={t("decrease-year")}
-                    title={t("decrease-year")}
-                    style={{
-                        height: "40px",
-                        width: "40px",
-                        borderRightColor: "transparent",
-                        borderBottomColor: "transparent",
-                        borderTopRightRadius: 0,
-                        borderBottomRightRadius: 0,
-                        borderBottomLeftRadius: 0,
-                    }}
-                    disabled={currentYear <= metadata.bois.start_year}
-                    onClick={() => handleYearChange(currentYear - 1)}
+            <div>
+                <div
+                    className="rows"
+                    style={{ gap: 0 }}
                 >
-                    {"<"}
-                </button>
+                    <button
+                        className="btn"
+                        type="button"
+                        aria-label={t("decrease-year")}
+                        title={t("decrease-year")}
+                        style={{
+                            height: "40px",
+                            width: "40px",
+                            borderRightColor: "transparent",
+                            borderBottomColor: "transparent",
+                            borderTopRightRadius: 0,
+                            borderBottomRightRadius: 0,
+                            borderBottomLeftRadius: 0,
+                        }}
+                        disabled={currentYear <= metadata.bois.start_year}
+                        onClick={() => handleYearChange(currentYear - 1)}
+                    >
+                        {"<"}
+                    </button>
 
-                <SelectMenu
-                    id="year-select"
-                    style={{
-                        borderRadius: 0,
-                        width: "100px",
-                        borderBottomColor: "transparent",
-                    }}
-                    options={Array.from(
-                        {
-                            length:
-                                metadata.bois.end_year -
-                                metadata.bois.start_year +
-                                1,
-                        },
-                        (_, i) => (metadata.bois.start_year + i).toString(),
-                    )}
-                    selectedOption={currentYear.toString()}
-                    onOptionSelect={(option: string) =>
-                        handleYearChange(parseInt(option, 10))
-                    }
+                    <SelectMenu
+                        id="year-select"
+                        style={{
+                            borderRadius: 0,
+                            width: "100px",
+                            borderBottomColor: "transparent",
+                        }}
+                        options={Array.from(
+                            {
+                                length:
+                                    metadata.bois.end_year -
+                                    metadata.bois.start_year +
+                                    1,
+                            },
+                            (_, i) => (metadata.bois.start_year + i).toString(),
+                        )}
+                        selectedOption={currentYear.toString()}
+                        onOptionSelect={(option: string) =>
+                            handleYearChange(parseInt(option, 10))
+                        }
+                    />
+                    <button
+                        className="btn"
+                        type="button"
+                        aria-label={t("increase-year")}
+                        title={t("increase-year")}
+                        onClick={() => handleYearChange(currentYear + 1)}
+                        disabled={currentYear >= metadata.bois.end_year}
+                        style={{
+                            height: "40px",
+                            width: "40px",
+                            borderLeftColor: "transparent",
+                            borderBottomColor: "transparent",
+                            borderTopLeftRadius: 0,
+                            borderBottomLeftRadius: 0,
+                            borderBottomRightRadius: 0,
+                        }}
+                    >
+                        {">"}
+                    </button>
+                </div>
+                <MonthSelector
+                    currentMonth={currentMonth}
+                    setCurrentMonth={setCurrentMonth}
                 />
-                <button
-                    className="btn"
-                    type="button"
-                    aria-label={t("increase-year")}
-                    title={t("increase-year")}
-                    onClick={() => handleYearChange(currentYear + 1)}
-                    disabled={currentYear >= metadata.bois.end_year}
-                    style={{
-                        height: "40px",
-                        width: "40px",
-                        borderLeftColor: "transparent",
-                        borderBottomColor: "transparent",
-                        borderTopLeftRadius: 0,
-                        borderBottomLeftRadius: 0,
-                        borderBottomRightRadius: 0,
-                    }}
-                >
-                    {">"}
-                </button>
             </div>
-            <MonthSelector
-                currentMonth={currentMonth}
-                setCurrentMonth={setCurrentMonth}
-            />
 
             <p>{t("product-choose")}</p>
             <button
                 className="btn"
                 type="button"
                 onClick={() => setIsOpenProducts(true)}
-                style={{ maxWidth: "clamp(180px, 30vw, 360px)" }}
+                style={{ maxWidth: "clamp(120px, 20vw, 240px)" }}
             >
                 {t("select-products")}
             </button>
@@ -385,6 +391,12 @@ export default function ConfigBar({
                                     key={countryNumberCode}
                                     className="tooltip-country"
                                     alt={t("flag", {
+                                        country:
+                                            locale === "en"
+                                                ? country.en
+                                                : country.fr || "unknown",
+                                    })}
+                                    title={t("flag", {
                                         country:
                                             locale === "en"
                                                 ? country.en
